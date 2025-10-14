45ec6b4c8c0069175e39b24baf78e38a406ec52a
Manuel Schneider <manuelschneid3r@gmail.com>
2025-10-14 13:02:47 +0200

    chore(release): v2.1.1

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import shutil
 from albert import *
 
 md_iid = "4.0"
-md_version = "2.1"
+md_version = "2.1.1"
 md_name = "GoldenDict"
 md_description = "Quick access to GoldenDict"
 md_license = "MIT"
